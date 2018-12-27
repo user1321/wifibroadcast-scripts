ecbece7e328d706b0e0923b01741d3715f1508a3
user1321 <33780612+user1321@users.noreply.github.com>
2018-12-28 00:39:29 +0200

    Update rc_tx_rx_functions.sh

--- rc_tx_rx_functions.sh
+++ rc_tx_rx_functions.sh
@@ -53,5 +53,7 @@ function rctx_function {
 	
     while true; do
     	nice -n -5 /tmp/rctx $NICS
+	NICS=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v relay | nice grep -v wifihotspot`
+	sleep 1
     done
 }
