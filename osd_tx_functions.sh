a73e995824dd487e819e098c874c6ca44102fbd5
user1321 <33780612+user1321@users.noreply.github.com>
2018-12-27 23:35:55 +0200

    Update osd_tx_functions.sh

--- osd_tx_functions.sh
+++ osd_tx_functions.sh
@@ -24,10 +24,11 @@ function osdtx_function {
     echo
     while true; do
         echo "Starting downlink telemetry transmission in $TXMODE mode (FC Serialport: $FC_TELEMETRY_SERIALPORT)"
-        nice cat $FC_TELEMETRY_SERIALPORT | nice /home/pi/wifibroadcast-base/tx_telemetry -p 1 -c $TELEMETRY_CTS -r 2 -x $TELEMETRY_TYPE -d 12 -y 0 $NICS
+        nice cat $FC_TELEMETRY_SERIALPORT | nice /home/pi/cameracontrol/RCParseCh $ChannelToListen | nice /home/pi/wifibroadcast-base/tx_telemetry -p 1 -c $TELEMETRY_CTS -r 2 -x $TELEMETRY_TYPE -d 12 -y 0 $NICS
         ps -ef | nice grep "cat $FC_TELEMETRY_SERIALPORT" | nice grep -v grep | awk '{print $2}' | xargs kill -9
         ps -ef | nice grep "tx_telemetry -p 1" | nice grep -v grep | awk '{print $2}' | xargs kill -9
-		
+	ps -ef | nice grep "RCParseCh" | nice grep -v grep | awk '{print $2}' | xargs kill -9
+	
 		echo "Downlink Telemetry TX exited - restarting ..."
 		
         sleep 1
