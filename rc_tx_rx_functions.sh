df8e668e84686d53fc336e3bda1794850a0de44f
user1321 <33780612+user1321@users.noreply.github.com>
2019-01-17 21:05:05 +0200

    FIX wrong NICS var

--- rc_tx_rx_functions.sh
+++ rc_tx_rx_functions.sh
@@ -60,7 +60,7 @@ function rctx_function {
     while true; do
     
     	if [ "$EncryptionOrRange" == "Range" ] || [ "$EncryptionOrRange" == "Encryption" ]; then
-		nice -n -5 /tmp/rctx $ChannelToListen2 $PrimaryCardMAC
+		nice -n -5 /tmp/rctx $NICS
 		NICS=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v relay | nice grep -v wifihotspot`
 	fi
 	
